fb6a27a5f4c79574e568062e97278eb7f9cdab64
Lucas Bäckvall <lucasba@student.chalmers.se>
2019-03-16 18:49:40 +0100

    Made server handle multiple connections via threading.

--- server.py
+++ server.py
@@ -1,15 +1,38 @@
 import socket
+import threading
 
+# Setup the socket handling the HTTP requests.
 s = socket.socket()
-
 s.bind(("", 80))
 s.listen(10)
 
-while True:
+# Global variables & constants
+running = True
+connections = []
 
-    c, addr = s.accept()
-    
-    c.send(str.encode("sug min röv azco"))
-    c.close()
+# Connection thread:         reserved,  function,           name,       args, kwargs
+tConnections = threading.Thread(None, fConnections, "Connections thread", (), {})
 
+# Thread functions
+def fConnections():
+	while running:
+		c, addr = s.accept()
+		tNewClientHandler = threading.Thread(None, fClientHandler, None, (c, addr), {})
+		tNewClientHandler.daemon = True
+		connections.append(c, addr)
+		tNewClientHandler.start()
+	for c, _ in connections:
+		c.close()
+
+def fClientHandler(c, addr):
+	while running:
+		inp = str.decode(c.recv(4096))
+		c.send(str.encode("Du skickade: " + inp))
+
+# Beginning of execution
+
+tConnections.start()
+# TODO: modify running variable here.
+tConnections.join()
+print("Safe program exit.")
 
