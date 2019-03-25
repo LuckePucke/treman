bbe5ed0323cbb658eb743d81a3cd57eab0ddf8a0
Lucas Bäckvall <lucas.backvall@gmail.com>
2019-03-25 05:15:22 +0100

    Server now fully separated into it's own object.

--- server.py
+++ server.py
@@ -1,116 +1,68 @@
+# Imports
 import socket
 import threading
-import sys
-import time
-import random
 
-# Read index file into memory
-try:
-    fIndex = open("index.html", "r")
-    index = fIndex.read()
-except:
-    print("couldn't open index.html")
-    sys.exit()
-
-# Setup the socket handling the HTTP requests.
-s = socket.socket()
-s.bind(("", 80))
-s.listen(10)
-
-# Global variables & constants
-running = True
-connections = {}
-connectionCounter = 0
-rooms = {}
-
-def responseHeader(len):
+class tcpServer(object):	
     
-    return "HTTP/1.1 200 OK\nDate: " + time.strftime("%a, %d %b %Y %H:%M:%S %Z") + "\nContent-Type: text/html; charset=UTF-8\nContent-Length: " + str(len) + "\nLast-Modified: " + time.strftime("%a, %d %b %Y %H:%M:%S %Z") + "\nServer: Python/3.5.3 (Unix) (Raspbian 2018-11-13)\nETag: \"" + time.strftime("%a %d %b %Y %H %M %S") + "\"\nAccept-Ranges: bytes\nConnection: close\n\n"
-
-# Thread functions
-def fConnections():
-    # Handles all connections.
-    # Inserts the connection into the connections directory with the address as the key.
-    global connections
-    global connectionCounter
-    while True:
-        try:
+    # Variables & constants
+    running = True
+    connections = {} # Dictionary {key: connection} where key <- connectionCounter
+    connectionCounter = 0
+    
+    
+    def __init__(self, clientHandlerFunction, port = 80, socketTimeout = None):
+        self.socketTimeout = socketTimeout
+        self.port = port
+        self.fClientHandler = clientHandlerFunction
+    
+    def recv(self, connectionID, size):
+        return self.connections[connectionID].recv(size)
+    
+    def send(self, connectionID, msg):
+        self.connections[connectionID].send(msg)
+    
+    def sendString(self, connectionID, string):
+        self.connections[connectionID].send(str.encode(string))
+    
+    def killConnection(self, connectionID):
+        self.connections[connectionID].close()
+        del self.connections[connectionID]
+    
+    # Thread functions
+    def fConnections(self):
+        # Handles all connections.
+        # Inserts the connection into the connections directory with the address as the key.
+        while self.running:
             # First we accept the new connection
-            c, addr = s.accept()
-            connections.update({connectionCounter: c})
-            tNewClientHandler = threading.Thread(None, fClientHandler, None, (connectionCounter, c, addr), {})
+            try:
+                c, addr = self.s.accept()
+            except:
+                continue
+            c.settimeout(self.socketTimeout)
+            self.connections.update({self.connectionCounter: c})
+            tNewClientHandler = threading.Thread(None, self.fClientHandler, None, (self, self.connectionCounter, addr), {})
             tNewClientHandler.start()
-            connectionCounter += 1
-        except err:
-            print("We had a failed connection: %s", err)
-
-class room(object):
-    def __init__(self):
-        self.diceA = 0
-        self.diceB = 0
-    def roll(self):
-        self.diceA = random.choice([1, 2, 3, 4, 5, 6])
-        self.diceB = random.choice([1, 2, 3, 4, 5, 6])
-    def dice(self):
-        return str(self.diceA) + " " + str(self.diceB)
-
-def fClientHandler(connectionID, c, addr):
-    global connections
-    global rooms
-    global running
-    global index
-    with c:
-        while running:
-            data = c.recv(4096)
-            if not data:
-                c.close()
-                del connections[connectionID]
-                break
-            sData = bytes.decode(data)
-            print("\n#" + str(connectionID) + " Recieved:\n" + sData)
-            if "text/html" not in sData:
-                c.send(str.encode("HTTP 404 Not found"))
-                c.close()
-                del connections[connectionID]
-                break
-            if "GET /treman HTTP" in sData:
-                c.send(str.encode(responseHeader(len(index)) + index))
-            elif "GET /treman?roomCode=" in sData:
-                roomCode = sData[21:25]
-                if roomCode not in rooms:
-                    rooms.update({roomCode: room()})
-                page = "<html><body>\n"
-                page += rooms[roomCode].dice()
-                page += "\n<form action=\"/treman\" method=\"GET\"><input type=\"submit\" name=\""+ roomCode +"\" value=\"Roll\"></form>"
-                page += "\n<form action=\"/treman\" method=\"GET\"><input type=\"submit\" name=\""+ roomCode +"\" value=\"Update\"></form>"
-                page += "\n</body></html>"
-                c.send(str.encode(responseHeader(len(page)) + page))
-            elif "GET /treman?" in sData:
-                roomCode = sData[12:16]
-                if sData[17:21] == "Roll":
-                    rooms[roomCode].roll()
-                page = "<html><body>\n"
-                page += rooms[roomCode].dice()
-                page += "\n<form action=\"/treman\" method=\"GET\"><input type=\"submit\" name=\""+ roomCode +"\" value=\"Roll\"></form>"
-                page += "\n<form action=\"/treman\" method=\"GET\"><input type=\"submit\" name=\""+ roomCode +"\" value=\"Update\"></form>"
-                page += "\n</body></html>"
-                c.send(str.encode(responseHeader(len(page)) + page))
-            else:
-                c.send(str.encode("HTTP 404 Not found"))
-                c.close()
-                del connections[connectionID]
-                break
+            self.connectionCounter += 1
+    
+    def start(self):
+        self.s = socket.socket()
+        self.s.settimeout(self.socketTimeout)
+        self.s.bind(("", self.port))
+        self.s.listen(10)
+        # Connection thread:         reserved,    function,             name,        args, kwargs
+        tConnections = threading.Thread(None, self.fConnections, "Connections thread", (), {})
+        #tConnections.daemon = True
+        tConnections.start()
+    
+    def isRunning(self):
+        return self.running
+    
+    def stop(self):
+        self.running = False
+        for _, c in self.connections.items():
+            c.close()
+        self.s.close()
+    
 
 
-
-# Connection thread:		 reserved,	function,			name,		args, kwargs
-tConnections = threading.Thread(None, fConnections, "Connections thread", (), {})
-tConnections.daemon = True
-tConnections.start()
-_ = input("Press enter to shutdown the server.")
-running = False
-for addr, c in connections.items():
-    c.close()
-s.close()
-print("Safe program exit.")
 
